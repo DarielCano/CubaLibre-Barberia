aac01574ad021def7324924f11803113e543600b
Dariel Cano <dariel.cano1992@gmail.com>
2023-01-22 00:39:13 -0600

    segunda preentrega

--- js/index.js
+++ js/index.js
@@ -1,49 +1,92 @@
-import { menu } from "./menu.js";
-import { scrollTop } from "./scroll.js";
-
-menu(".menu-btn", ".nav");
-scrollTop(".header", ".nav");
-
 /* SIMULANDO BARBERIA, CON CITAS Y CON TIENDA DE PRODUCTOS INCLUIDA */
 
+/* declaracion de clases */
 class Usuario {
   constructor(nombre, apellido, edad) {
     (this.nombre = nombre), (this.apellido = apellido), (this.edad = edad);
   }
+
+  consultarEdad() {
+    if (this.edad < 18) return "menor";
+    else return "mayor";
+  }
 }
 
+class Usuario_barberia extends Usuario {
+  constructor(nombre, apellido, edad, servicios) {
+    super(nombre, apellido, edad);
+    this.servicios = servicios;
+  }
+
+  agregarServicio(servicio) {
+    this.servicios.push(servicio);
+  }
+
+  eliminarServicio(servicio) {
+    /*    console.log(this.servicios.findIndex((element) => element == servicio)); */
+    if (this.servicios.findIndex((element) => element == servicio) == -1) {
+      return false;
+    } else {
+      this.servicios = this.servicios.filter((element) => element != servicio);
+      return true;
+    }
+  }
+
+  mostrarServicios = () => {
+    if (this.servicios.length == 0) return `No tiene citas agendadas aún`;
+    else return this.servicios;
+  };
+}
+
+class Producto {
+  constructor(nombre, precio, stock) {
+    (this.nombre = nombre), (this.precio = precio), (this.stock = stock);
+  }
+
+  actualizarStock(comprado) {
+    this.stock = this.stock - comprado;
+  }
+
+  calcularMonto(comprado) {
+    return comprado * this.precio;
+  }
+}
+///////////////////////////////////////////////////////////////////////////////////
+
+/* declaracion de arreglo de productos en tienda
+(Se agregan hipoteticamente del backend(base de datos)) */
+let productos = [
+  new Producto("Cera para peinar", 250, 10),
+  new Producto("Maquina Trimmer", 1000, 10),
+  new Producto("Shaving gel", 150, 10),
+  new Producto("Tinte para barba", 300, 10),
+];
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+/* declaracion de variables */
 let terminar = "no";
 let op = "";
 let fin = "";
-let citas = [];
-
-/* productos en stock y su disponibilidad */
-let cantCeras = 10;
-let cantGel = 10;
-let cantMaq = 10;
-let cantTinte = 10;
 let cant = 0;
 let carrito = [];
+let carrito_prod = [];
 
-/* funciones corte y cita */
-const consultarEdad = (edad) => {
-  if (edad < 18) return "menor";
-  else return "mayor";
-};
+let prod;
 
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* funciones Servicios de corte */
 const respuesta = (tipoCorte) => {
   fin = prompt(
     `Ha elegido ${tipoCorte}, presione cualquier tecla para confirmar o escriba NO/no para cancelar`
   );
   if (fin.toLowerCase() != "no") {
-    agregarCita(`${tipoCorte}`);
+    clienteCorte.agregarServicio(`${tipoCorte}`);
     alert(`Cita guardada`);
   } else {
-    eliminarCita();
     alert(`Cita Cancelada`);
   }
 };
-
 const serviciosMenor = () => {
   op = prompt(`Inserte la opción segun el servicio al menor:
                    1 - Corte sencillo -- Precio $100
@@ -61,7 +104,6 @@ const serviciosMenor = () => {
     respuesta("corte moderno");
   }
 };
-
 const servicioMayor = () => {
   op = prompt(`Inserte la opción segun el servicio:
                    1 - Corte sencillo -- Precio $100
@@ -101,32 +143,59 @@ const servicioMayor = () => {
   }
 };
 
-const agregarCita = (cita) => citas.push(cita);
-
-const eliminarCita = () => {
-  if (citas.length != 0) {
-    citas.pop();
-  }
-};
-
-const mostrarCitas = () => {
-  if (citas.length == 0) return `No tiene citas agendadas aún`;
-  else return citas;
-};
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 /* funciones venta productos barberia */
 const articulosEnVenta = () => {
   alert(`Acabas de ingresar a nuestra tienda`);
-  let producto = prompt(`Contamos con los siguientes productos:
-  1 - Cera para peinar -- cantidad: ${cantCeras}
-  2 - Máquina trimmer -- cantidad: ${cantMaq}
-  3 - Shaving gel -- cantidad: ${cantGel}
-  4 - Tinte para barba -- cantidad: ${cantTinte} 
-  ELIJA SU PRODUCTO
-  
+  let string = `Contamos con los siguientes productos:`;
+  let contador = 1;
+  for (let elemento of productos) {
+    string =
+      string +
+      `
+      ${contador}- ${elemento.nombre} - Precio: ${elemento.precio} - Cantidad: ${elemento.stock}`;
+    contador += 1;
+  }
+  let producto = prompt(`${string}
+  ELIJA SU PRODUCTO 
   --Para eliminar el último producto de su carrito presione 5--
-  6 - SALIR`);
+    6 - SALIR`);
   return producto;
+};
+
+const agregarAlCarrito = (prod_carrito) => {
+  carrito.push(prod_carrito);
+};
+
+const eliminarCarrito = () => {
+  if (carrito.length != 0) {
+    let eliminar = prompt(`Tiene su carrito con los siguientes productos:
+   ${mostrarProductos()}
+   Escriba el producto a eliminar`);
+    eliminar = eliminar.toLowerCase();
+
+    if (
+      carrito.findIndex(
+        (element) => element.nombre.toLowerCase() == eliminar
+      ) == -1
+    ) {
+      alert(`EL producto ${eliminar} no existe en su carrito, `);
+    } else {
+      carrito = carrito.filter(
+        (element) => element.nombre.toLowerCase() != eliminar
+      );
+      alert(
+        `Carrito actualizado. Tiene los siguientes productos en su carrito:
+         ${mostrarProductos()}`
+      );
+    }
+  }
+};
+
+const mostrarProductos = () => {
+  carrito_prod = carrito.map((element) => element.nombre);
+  return carrito_prod;
 };
 
 const seleccionCarrito = (prod) => {
@@ -142,15 +211,19 @@ const seleccionCarrito = (prod) => {
                 Escriba la cantidad de ceras a comprar:`)
         );
       }
-      if (cant > cantCeras) {
-        alert(`Existen ${cantCeras} en almacén. 
+      if (cant > productos[0].stock) {
+        alert(`Existen ${productos[0].stock} en almacén. 
             Para otra compra vuelva a accedera nuestra tienda. Una disculpa`);
       } else {
-        cantCeras -= cant;
+        productos[0].actualizarStock(cant);
         alert(
-          `gracias por su compra. Tenemos disponibilidad ahora de ${cantCeras}`
+          `Gracias por su compra.
+          Precio a pagar: ${productos[0].calcularMonto(cant)}
+           Tenemos disponibilidad ahora de ${productos[0].stock}`
         );
-        agregarAlCarrito("Ceras para peinar");
+        console.log(productos[0]);
+
+        agregarAlCarrito(productos[0]);
       }
 
       break;
@@ -165,15 +238,18 @@ const seleccionCarrito = (prod) => {
                       Escriba la cantidad de máquinas trimmer a comprar:`)
         );
       }
-      if (cant > cantMaq) {
-        alert(`Existen ${cantMaq} en almacén. 
+      if (cant > productos[1].stock) {
+        alert(`Existen ${productos[1].stock} en almacén. 
                   Para otra compra vuelva a accedera nuestra tienda. Una disculpa`);
       } else {
-        cantMaq -= cant;
+        productos[1].actualizarStock(cant);
+
         alert(
-          `Gracias por su compra. Tenemos disponibilidad ahora de ${cantMaq}`
+          `Gracias por su compra.
+           Precio a pagar: ${productos[1].calcularMonto(cant)}
+           Tenemos disponibilidad ahora de ${productos[1].stock}`
         );
-        agregarAlCarrito("Máquinas trimmer");
+        agregarAlCarrito(productos[1]);
       }
 
       break;
@@ -189,15 +265,17 @@ const seleccionCarrito = (prod) => {
                       Escriba la cantidad de shaving gel a comprar:`)
         );
       }
-      if (cant > cantGel) {
-        alert(`Existen ${cantGel} en almacén. 
+      if (cant > productos[2].stock) {
+        alert(`Existen ${productos[2].stockl} en almacén. 
                   Para otra compra vuelva a accedera nuestra tienda. Una disculpa`);
       } else {
-        cantGel -= cant;
+        productos[2].actualizarStock(cant);
         alert(
-          `gracias por su compra. Tenemos disponibilidad ahora de ${cantGel}`
+          `Gracias por su compra.
+           Precio a pagar: ${productos[2].calcularMonto(cant)}
+           Tenemos disponibilidad ahora de ${productos[2].stock}`
         );
-        agregarAlCarrito("Shaving gel");
+        agregarAlCarrito(productos[2]);
       }
 
       break;
@@ -213,15 +291,18 @@ const seleccionCarrito = (prod) => {
                     Escriba la cantidad de tinte  para barba a comprar:`)
         );
       }
-      if (cant > cantTinte) {
-        alert(`Existen ${cantTinte} en almacén. 
+      if (cant > productos[3].stock) {
+        alert(`Existen ${productos[3].stock} en almacén. 
                 Para otra compra vuelva a accedera nuestra tienda. Una disculpa`);
       } else {
-        cantTinte -= cant;
+        productos[3].actualizarStock(cant);
+        /* cantTinte -= cant; */
         alert(
-          `gracias por su compra. Tenemos disponibilidad ahora de ${cantTinte}`
+          `Gracias por su compra.
+          Precio a pagar: ${productos[3].calcularMonto(cant)}
+           Tenemos disponibilidad ahora de ${productos[3].stock}`
         );
-        agregarAlCarrito("Tinte para barba");
+        agregarAlCarrito(productos[3]);
       }
 
       break;
@@ -231,9 +312,9 @@ const seleccionCarrito = (prod) => {
       break;
 
     case "6":
-      alert(`Hssta la proxima. Presione aceptar para ver su carrito`);
+      alert(`Hasta la proxima. Presione aceptar para ver su carrito`);
       if (carrito.length == 0) alert(`No tiene productos en su carrito`);
-      else alert(`Productos de su carrito ---  ${carrito}`);
+      else alert(`Productos de su carrito --- ${mostrarProductos()}`);
       break;
     default:
       alert(`No ha elegido ninguna opcion anterior. 
@@ -242,17 +323,12 @@ const seleccionCarrito = (prod) => {
   }
 };
 
-const agregarAlCarrito = (elemento) => {
-  carrito.push(elemento);
-};
-
-const eliminarCarrito = () => {
-  if (carrito.length != 0) carrito.pop();
-};
-
 /////////////////////////////////////////////////////////////////////////////////////////
+
+/* Entrada de datos */
 let nombre = prompt(`Bienvenido a CubaLibre Barbería.
 Inserte sus datos. Nombre: `);
+
 while (!nombre || isNaN(nombre) == false) {
   nombre = prompt(`Campo vacio o numero.
     Inserte su Nombre: `);
@@ -267,7 +343,21 @@ while (isNaN(edad) || edad < 1 || edad > 120) {
   edad = parseInt(prompt(`Inserte edad valida`));
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+/* declaracion de objetos cliente y cliente corte */
+const cliente = new Usuario(nombre, apellido, edad);
+const clienteCorte = new Usuario_barberia(
+  cliente.nombre,
+  cliente.apellido,
+  cliente.edad,
+  []
+);
+
+////////////////////////////////////////////////////////////////////////////////
+
 /* ciclo principal del programa */
+
 while (terminar == "no") {
   let opcion = prompt(`
   
@@ -279,16 +369,36 @@ while (terminar == "no") {
 
   switch (opcion) {
     case "1":
-      let res = consultarEdad(edad);
+      let res = cliente.consultarEdad();
       if (res == "menor") {
-        alert(`Es usted menor de edad, por lo que cuenta con sus servcios diferenciados.
+        alert(`Es usted menor de edad, por lo que cuenta con sus servicios diferenciados.
                Presione Aceptar para continuar`);
+
         serviciosMenor();
       } else servicioMayor();
       break;
 
     case "2":
-      alert(`Usted tiene la(s) siguiente(s) cita(s) -- ${mostrarCitas()}`);
+      alert(
+        `Usted tiene lo(s) siguiente(s) servicios agendados(s) -- ${clienteCorte.mostrarServicios()}`
+      );
+      if (clienteCorte.servicios.length != 0) {
+        let cancelar = prompt(
+          `Escriba el servicio que desea cancelar o no/NO para salir:`
+        );
+        cancelar = cancelar.toLowerCase();
+        console.log(cancelar);
+        if (cancelar !== "no") {
+          let result = clienteCorte.eliminarServicio(cancelar);
+          if (result == true) {
+            alert(`Servicio eliminado.
+            Usted tiene lo(s) siguiente(s) servicios agendados(s) -- ${clienteCorte.mostrarServicios()}`);
+          } else {
+            alert(`Servicio no existente`);
+          }
+        }
+      }
+
       break;
 
     case "3":
@@ -303,3 +413,10 @@ while (terminar == "no") {
       break;
   }
 }
+
+/////////////////////////////////////////////////////////////////////////////////
+import { menu } from "./menu.js";
+import { scrollTop } from "./scroll.js";
+
+menu(".menu-btn", ".nav");
+scrollTop(".header", ".nav");
